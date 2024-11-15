2c0a8e7cba2a9b6740d2fca66d31006419410e31
Abhinav Anand <abhinav011501@gmail.com>
2024-11-16 04:14:46 +0530

    Add introductory examples and functions for string manipulation in Python

---
+++ chapter_3/01_intro_to_string.py
@@ -0,0 +1,16 @@
+# Intro to String
+
+# String is a data type in python which is used to store text data.
+# String is a sequence of characters. In python, we can create string using single quote, double quote or triple quote. 
+# String is a immutable data type in python. It means we can't change the value of string once it is created.
+
+a = "Hello, World!" # Double Quote String
+
+b = 'Hello, World!' # Single Quote String
+
+c = '''Hello, World!''' # Multiline String
+
+#all the above strings are same and can be used interchangeably in python code.
+
+
+
