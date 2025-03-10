8cb58cd9d5737f04cb3954b604bd0d7300271336
Abhinav <abhinav011501@gmail.com>
2025-03-10 14:21:06 +0530

    Remove chapter 8

--- chapter_8-function-n-recursion/04_function_with_return_keyword.py
+++
@@ -1,8 +0,0 @@
-
-def greet(name, message):
-    """This function greets to the person passed in as parameter"""
-    return "Hello, " + name + ". " + message
-
-
-res = greet('Rahul', "Good night!")  # Output: Hello, Paul. Good night!
-print(res)
